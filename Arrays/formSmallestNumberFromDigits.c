59c5426b8f4727e6b2cc151a90a6d0960d20e8ff
ilaur <ilaur@gmail.com>
2018-04-23 23:26:12 +0300

    Adding problem

--- Arrays/formSmallestNumberFromDigits.c
+++ Arrays/formSmallestNumberFromDigits.c
@@ -8,12 +8,27 @@ Example : If n is 52200996 the smallest number is 20025699
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 void main() {
-	unsigned long num, *splitNum = NULL;
-	int size = 0;
+	int num, i, j;
+	int *arr = (int*)malloc(10*sizeof(int));
+	for(i=0; i<10; i++) 
+		*(arr+i) = 0;
 	printf("Please enter a number : ");
-	scanf("%lu", &num);
-	while(num/10)
+	scanf("%d", &num);
+	while(num>0) {
+		arr[num%10]++;
+		num/=10;
+	}
+	i = 1;
+	while(arr[i]==0)
+		i++;
+	num = i;
+	arr[i]--;
+	for(i=0; i<10; i++)
+		for(j=1; j<=arr[i]; j++)
+			num = num*10+i;
+	printf("%d", num);
+	free(arr);
+	puts("");
 }
